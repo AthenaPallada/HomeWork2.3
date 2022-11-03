54cfb30f480f036fd5446c87826a6239c7fe0fd4
tatiana_alekseeva <tatianaalekseeva1313@gmail.com>
2022-11-03 19:54:03 +0300

    Делаю пул-реквест

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача 6
+        // Задача 6
         System.out.println("Задача 6");
         var boxer1 = 78.2;
         var boxer2 = 82.7;
         System.out.println("Общий вес двух бойцов - " + (boxer1 + boxer2) + "кг");
         System.out.println("Разница между весами 2-х бойцов - " + (boxer2 - boxer1) + "кг");
 
-        //Задача 7
+        // Задача 7
         System.out.println("Задача 7");
         System.out.println("Разница между весами 2-х бойцов(1способ) - " + (boxer2 - boxer1) + "кг");
         System.out.println("Разница между весами 2-х бойцов(2способ) - " + (boxer2 % boxer1) + "кг");
 
-        //Задача 8
+        // Задача 8
         System.out.println("Задача 8");
         var totalHours = 640;
         var workHours = 8;
